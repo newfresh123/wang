e92dcac97a8384484f9e76af703cc2fcd0f1c788
Hilary Liu <2788370451@qq.com>
2023-09-20 17:33:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# vue3-template-starter
+
+This template should help get you started developing with Vue 3 in Vite.
+
+- CORE vue3 + typescript + vite
+- ROUTER vue-router
+- CSS tailwind.css + sass
+- ICON unplugin-icons
+
+## Project Setup
+
+```sh
+pnpm i
+pnpm dev
+```
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import MdiFormatHeader1 from '~icons/mdi/format-header-1'
+</script>
+
+<template>
+  <div class="bg-red-50">
+    hello world !!
+    <br />
+    ICON
+    <MdiFormatHeader1></MdiFormatHeader1>
+  </div>
+</template>
+<style lang="scss"></style>
